d902fe29f2ffa4540419412202e08e33bb4983a2
khadziabdi1 <khadziabdi1@etf.unsa.ba>
2024-03-24 19:46:34 +0100

    Enabling openapi and swagger ui

--- records_handler/src/main/java/ba/unsa/etf/nwt/RecordsHandlerApplication.java
+++ records_handler/src/main/java/ba/unsa/etf/nwt/RecordsHandlerApplication.java
@@ -1,28 +1,12 @@
 package ba.unsa.etf.nwt;
 
-import ba.unsa.etf.nwt.model.RecordEntity;
-import ba.unsa.etf.nwt.repository.RecordsRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RecordsHandlerApplication implements CommandLineRunner {
-	@Autowired
-	public RecordsRepository recordsRepository;
+public class RecordsHandlerApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(RecordsHandlerApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		RecordEntity record = new RecordEntity();
-		record.setId(1);
-		record.setPatient(null);
-		record.setImage("3");
-		record.setAppointment(null);
-
-		recordsRepository.save(record);
-	}
 }
