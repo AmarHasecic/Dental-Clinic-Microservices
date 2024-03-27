e32515e178f96ce0b3d3227d92d80e1207ec4650
AmarHasecic <ahasecic1@etf.unsa.ba>
2024-03-27 16:43:07 +0100

    Dependencies fixed

--- appointment_handler/src/main/java/ba/unsa/etf/nwt/AppointmentHandlerApplication.java
+++ appointment_handler/src/main/java/ba/unsa/etf/nwt/AppointmentHandlerApplication.java
@@ -1,18 +1,10 @@
 package ba.unsa.etf.nwt;
 
-import ba.unsa.etf.nwt.model.AppointmentEntity;
-import ba.unsa.etf.nwt.repository.AppointmentsRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Date;
-
 @SpringBootApplication
 public class AppointmentHandlerApplication {
-	@Autowired
-	public AppointmentsRepository appointmentsRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(AppointmentHandlerApplication.class, args);
 	}
