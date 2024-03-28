27689790d6fbf69669c42b0096d7705f877fc4a6
AmarHasecic <ahasecic1@etf.unsa.ba>
2024-03-28 12:53:25 +0100

    DentistController deleted and AppointmentsController implemented instead

---
+++ appointment_handler/src/main/java/ba/unsa/etf/nwt/controller/AppointmentController.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.nwt.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/appointments")
+public class AppointmentEntity {
+
+
+
+}
