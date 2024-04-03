6b7efd84865cb95f955585f68ae8f8319ce30aed
khadziabdi1 <khadziabdi1@etf.unsa.ba>
2024-04-03 20:58:41 +0200

    Added eureka discovery service

--- records_handler/src/main/java/ba/unsa/etf/nwt/RecordsHandlerApplication.java
+++ records_handler/src/main/java/ba/unsa/etf/nwt/RecordsHandlerApplication.java
@@ -2,8 +2,10 @@ package ba.unsa.etf.nwt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RecordsHandlerApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(RecordsHandlerApplication.class, args);
