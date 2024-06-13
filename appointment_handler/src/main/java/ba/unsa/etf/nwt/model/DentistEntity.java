f22d7d35a2c0de5c1ea3e0cca2ae501a4612d21b
khadziabdi1 <khadziabdi1@etf.unsa.ba>
2024-06-13 02:37:32 +0200

    Update DentistEntity.java

--- appointment_handler/src/main/java/ba/unsa/etf/nwt/model/DentistEntity.java
+++ appointment_handler/src/main/java/ba/unsa/etf/nwt/model/DentistEntity.java
@@ -1,48 +1,39 @@
 package ba.unsa.etf.nwt.model;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.io.Serial;
 import java.io.Serializable;
 
+@Setter
+@Getter
 @Entity
 @Table(name = "dentists")
-@JsonIdentityInfo(generator = ObjectIdGenerators.IntSequenceGenerator.class, property = "@id", scope = DentistEntity.class)
 public class DentistEntity implements Serializable {
+    @Serial
+    private static final long serialVersionUID = -2731425678149216052L;
 
     @Id
-    //@GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    private String workHours;
 
-    public DentistEntity(long id, String workHours) {
-        this.id = id;
-        this.workHours = workHours;
-    }
+    @NotBlank(message = "Work hours must not be blank")
+    @Pattern(regexp = "^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]\\s?-\\s?(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$", message = "Invalid work hours format")
+    private String workingHours;
 
     public DentistEntity() {
     }
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getWorkHours() {
-        return workHours;
-    }
-
-    public void setWorkHours(String workHours) {
-        this.workHours = workHours;
+    public DentistEntity(String workHours) {
+        this.workingHours = workHours;
     }
 
     @Override
     public java.lang.String toString() {
-        return "{id: "+ this.id + ",workHours:"+this.workHours+"}";
+        return "{id: "+ this.id + ",workHours:"+this.workingHours+"}";
     }
+
 }
