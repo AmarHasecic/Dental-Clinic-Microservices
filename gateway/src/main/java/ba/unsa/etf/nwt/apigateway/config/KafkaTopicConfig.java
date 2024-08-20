b6a649b92801a3c447134f7e56d20855c2df2758
Amar Hasecic <ahasecic012@gmail.com>
2024-08-20 22:39:07 +0200

    - kafka server config - docker compose edit (kafka config) - test code that sends message to the broker

---
+++ gateway/src/main/java/ba/unsa/etf/nwt/apigateway/config/KafkaTopicConfig.java
@@ -0,0 +1,28 @@
+package ba.unsa.etf.nwt.apigateway.config;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        String bootstrapAddress = "broker:9092";
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        return new KafkaAdmin(configs);
+    }
+
+
+    @Bean
+    public NewTopic topic() {
+        return new NewTopic("logs", 1, (short) 1);
+    }
+}
--- gateway/src/main/java/ba/unsa/etf/nwt/apigateway/filter/RequestLoggingFilter.java
+++ gateway/src/main/java/ba/unsa/etf/nwt/apigateway/filter/RequestLoggingFilter.java
@@ -1,23 +1,30 @@
 package ba.unsa.etf.nwt.apigateway.filter;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.annotation.Order;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+import java.time.LocalDateTime;
 
 @Component
 @Order(1)
 public class RequestLoggingFilter implements GlobalFilter {
 
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+
         @Override
         public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
 
             String path = exchange.getRequest().getURI().getPath();
-
             if (path.equals("/auth/health")) {
-                System.out.println("Health check attempt detected: " + path);
+                String msg = LocalDateTime.now() + ": Health check attempt detected!";
+                kafkaTemplate.send("logs", msg);
             }
 
             return chain.filter(exchange);
